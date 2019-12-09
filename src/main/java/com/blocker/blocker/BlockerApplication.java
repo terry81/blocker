88a9af476bd6a4980a9c43ee272b71975a6e48e5
Anatoliy Dimitrov <tollodim@gmail.com>
2019-12-09 22:10:08 +0200

    Remove the unneeded test

--- src/main/java/com/blocker/blocker/BlockerApplication.java
+++ src/main/java/com/blocker/blocker/BlockerApplication.java
@@ -9,35 +9,37 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.Async;
 
 @SpringBootApplication
 public class BlockerApplication {
 
-	@Value("${blocker.honeypot_port}")
-	private int port;
-	
-	@Autowired
-	private IPLogger ipDropper;
+    @Value("${blocker.honeypot_port}")
+    private int port;
 
-	@Autowired
-	private IPAddressFormatValidator ipValidator;
+    @Autowired
+    private IPLogger ipDropper;
 
-	public static void main(String[] args) {
-		SpringApplication.run(BlockerApplication.class, args);
-	}
+    @Autowired
+    private IPAddressFormatValidator ipValidator;
 
-	@Bean
-	public void listener() throws IOException {
-		while (true) {
-			ServerSocket sSock = new ServerSocket(port);
-			Socket cSocket = sSock.accept();
+    public static void main(String[] args) {
+        SpringApplication.run(BlockerApplication.class, args);
+    }
+
+    @Bean
+    @Async
+    public void listener() throws IOException {
+        while (true) {
+            ServerSocket sSock = new ServerSocket(port);
+            Socket cSocket = sSock.accept();
             String ipAndPort[] = cSocket.getRemoteSocketAddress().toString().split(":");
             String ip = ipAndPort[0].replace("/", "");
-			cSocket.close();
-			sSock.close();
-			if (ipValidator.validate(ip)) {
-				ipDropper.log(ip);
-			}
-		}
-	}
+            cSocket.close();
+            sSock.close();
+            if (ipValidator.validate(ip)) {
+                ipDropper.log(ip);
+            }
+        }
+    }
 }
--- src/test/java/com/blocker/blocker/BlockerApplicationTests.java
+++ src/test/java/com/blocker/blocker/BlockerApplicationTests.java
@@ -1,13 +1,8 @@
 package com.blocker.blocker;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BlockerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
